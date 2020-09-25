65e46fe18155901b08f6af9dd8eaa7fef8228d8d
Alvaro Neto <alvaroshn@gmail.com>
2020-09-25 12:21:32 -0300

    movie library react

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -11,7 +11,7 @@ export const getMovies = () => (
     setTimeout(() => {
       const movies = readMovies();
       resolve(movies);
-    }, 2000);
+    }, 200);
   })
 );
 
@@ -21,7 +21,7 @@ export const getMovie = (movieId) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(movie);
-    }, 2000);
+    }, 200);
   });
 };
 
@@ -37,7 +37,7 @@ export const updateMovie = (updatedMovie) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve('OK');
-    }, 1000);
+    }, 200);
   });
 };
 
@@ -51,7 +51,7 @@ export const createMovie = (movieData) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve('OK');
-    }, 1000);
+    }, 200);
   });
 };
 
@@ -63,6 +63,6 @@ export const deleteMovie = (movieId) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({ status: 'OK' });
-    }, 1000);
+    }, 200);
   });
 };
