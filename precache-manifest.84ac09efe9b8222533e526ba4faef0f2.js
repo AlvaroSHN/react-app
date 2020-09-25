7625509305bdc9d4dd4928b975b5d0b3b936e7cf
Alvaro Neto <alvaroshn@gmail.com>
2020-09-25 11:50:46 -0300

    Updates

---
+++ precache-manifest.84ac09efe9b8222533e526ba4faef0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a29068c7356e3b799c7a3870e82f777",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "057d2a16e2220e7e2229",
+    "url": "/react-app/static/css/main.bcbaef4d.chunk.css"
+  },
+  {
+    "revision": "71c97a0001fb72b4e97d",
+    "url": "/react-app/static/js/2.0296dbad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.0296dbad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057d2a16e2220e7e2229",
+    "url": "/react-app/static/js/main.666619b3.chunk.js"
+  },
+  {
+    "revision": "65e9fa7dde63ea369bed",
+    "url": "/react-app/static/js/runtime-main.e03019ca.js"
+  }
+]);
